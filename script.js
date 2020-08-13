dfd41a000378d3d015de81fc5fe20be9f1c492db
Hunter <hunterkb@ksu.edu>
2020-08-13 13:11:04 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+
+button.onclick = function() {
+    let helloworld = document.createElement("p");
+    helloworld.textContent = "Hello world!";
+    helloworld.classList.add("hello");
+    body.appendChild(helloworld);
+}
