7b835b9fdfb67da0bbaad9e21d22e8fa0daf9460
Hunter <hunterkb@ksu.edu>
2020-08-14 15:55:54 -0500

    Testing callback function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("button");
 const body = document.querySelector("body");
+const postlist = document.querySelector("ul");
 
 button.onclick = function() {
     let helloworld = document.createElement("p");
@@ -8,3 +9,30 @@ button.onclick = function() {
     body.appendChild(helloworld);
     console.log("Test worked!");
 }
+
+const posts = [
+    {title: "Post One", body: "This is post one."},
+    {title: "Post Two", body: "This is post two."}
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = "";
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        postlist.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+getPosts();
+
+createPost({title: "Post Three", body: "This is post three."}, getPosts);
+
